7fb00f737594e654d9d97d7e4d830a3dee4d0fd7
Suleiman <mdsuleiman.edu@gmail.com>
2026-08-24 20:26:03 +0530

    Day15

--- src/components/AboutSection/AboutSection.jsx
+++ src/components/AboutSection/AboutSection.jsx
@@ -5,7 +5,7 @@ function AboutSection() {
     <section id='about' className='about-section'>
       <div className='about-wrapper container'>
         <div className='about-image'>
-          <img src='/profile.jpg' alt='Soumik Datta' />
+          <img src='/profile.jpg' alt='Suleiman' />
         </div>
         <div className='about-copy'>
           <h1>About Me</h1>
